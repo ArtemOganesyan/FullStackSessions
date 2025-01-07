17f373e87d5064caf77b33bf0dbaa77007244891
Artem Oganesyan <artem.oganesyan@gmail.com>
2025-01-06 19:29:07 -0800

    First update

--- src/Session12/BankingSystem/BankSystem.java
+++ src/Session12/BankingSystem/BankSystem.java
@@ -27,6 +27,34 @@ public class BankSystem {
             // savings
         }
 
+        while (true) {
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check balance");
+            System.out.println("4. Account details");
+            System.out.println("5. Exit");
+            System.out.println("Choose an option: ");
+            int option = scanner.nextInt();
 
+            switch (option) {
+                case 1:
+                    System.out.println("Enter deposit amount: ");
+                    double depositAmount = scanner.nextDouble();
+                    account.deposit(depositAmount);
+                    break;
+                case 2:
+                    System.out.println("Enter withdraw amount: ");
+                    double withdrawAmount = scanner.nextDouble();
+                    account.withdraw(withdrawAmount);
+                    break;
+                case 3:
+                    System.out.println("Current balance: " + account.getBalance());
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+        }
     }
 }
